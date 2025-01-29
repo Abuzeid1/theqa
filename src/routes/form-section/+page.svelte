e251918f916f2ed589800a828eaf9d8bc31513af
Ahmed Abuzeid <abuzeid1999@gmail.com>
2025-01-29 13:24:56 +0200

    form

---
+++ src/routes/form-section/+page.svelte
@@ -0,0 +1,50 @@
+<div class="px-section-secondary">
+    <div class="px-form-container">
+        <h2 class="px-header-secondary-brand px-text-center">
+            التسجيل في خدمة ادارة الأصول
+        </h2>
+        <form class="px-form">
+            <label class="px-label">
+                الاسم كاملا
+                <input
+                    class="px-input-primary"
+                    required
+                    type="text"
+                    placeholder="الاسم كاملا.."
+                />
+            </label>
+            <label class="px-label">
+                البريد الالكتروني
+                <input
+                    class="px-input-primary px-ltr-end"
+                    required
+                    type="email"
+                    placeholder="youremail@example.com"
+                />
+            </label>
+            <label class="px-label">
+                رقم الجوال
+                <input
+                    class="px-input-primary px-ltr-end"
+                    required
+                    type="tel"
+                    placeholder="(555) 052-1123"
+                />
+                <select class="px-input-primary">
+                    <option value="966" dir="ltr">
+                        &#x1F1F8;&#x1F1E6; +966</option
+                    >
+                </select>
+            </label>
+            <label class="px-label-checkbox">
+                أوافق على مشاركة بياناتي مع فريق ثقة واستخدام البيانات في
+                استقبال رسائل لها علاقة بخدمة ادارة المحفظة الاستثمارية
+                <input type="checkbox" class="px-checkbox-primary" required />
+            </label>
+            <button type="submit" class="px-button-primary">
+                تسجيل
+                <img src="/arrow-left.svg" alt="" />
+            </button>
+        </form>
+    </div>
+</div>
